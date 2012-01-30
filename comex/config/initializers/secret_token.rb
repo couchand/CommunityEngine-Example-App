7abb8a661ffe45553199f61e75ca154221b595f6
Andrew Couch <andy@couchand.com>
2012-01-29 19:29:42 -0600

    rails new comex -d postgresql

---
+++ comex/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comex::Application.config.secret_token = '591df085f65f8d846e94c4e85a11f50251b5fe9f0c862f807b435a9560b694ea1873469072bad3ab5947aaa4aebb01e8449f5fd158f051ce8405c27ba96441c2'
